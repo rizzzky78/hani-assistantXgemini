90951a477f34cc2ed4ad3c8dfc6c5c88f01e8df0
rizzzky78 <agungprasetyo9957@gmail.com>
2024-08-01 11:02:09 +0700

    revise Gemini Persona

--- controllers/gemini/ai-fileapi/index.js
+++ controllers/gemini/ai-fileapi/index.js
@@ -1,8 +1,12 @@
-const { GoogleAIFileManager } = require("@google/generative-ai/files");
+const {
+  GoogleAIFileManager,
+  FileState,
+} = require("@google/generative-ai/files");
 
 const { readFileSync, writeFileSync } = require("fs");
 const cryptoRandomStr = require("crypto-random-string");
 const logger = require("@libs/utils/logger");
+const { MIMEType } = require("util");
 
 class GeminiFileAPI {
   /**
@@ -21,24 +25,39 @@ class GeminiFileAPI {
    * @param { Buffer } fileBuffer
    * @param { string } mime
    */
-  fileProcessor(fileBuffer, mime) {
-    const filename = `./controllers/gemini/ai-fileapi/dist-fileapi/${cryptoRandomStr(
-      7
-    )}.${mime}`;
-    writeFileSync(filename, fileBuffer);
-    readFileSync(filename) && logger.info("File Processing: OK");
-    return filename;
+  async fileProcessor(fileBuffer, mime) {
+    const filePath = `./controllers/gemini/ai-fileapi/dist-fileapi/`;
+    const fileName = `${cryptoRandomStr(7)}.${mime}`;
+    const filePathName = filePath + fileName;
+    writeFileSync(filePathName, fileBuffer);
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    readFileSync(filePathName) && logger.info("File Processing: OK");
+    return { filePath: filePathName, fileName: fileName };
   }
 
   /**
    *
    * @param { Buffer } fileBuffer
+   * @param { MIMEType } mime
    */
-  async uploadFile(fileBuffer) {
-    return await this.fileManager.uploadFile("FILEPATH", {
-      mimeType: "MIME",
-      displayName: "DISPLAY_NAME",
+  async uploadFile(fileBuffer, mime) {
+    const { filePath, fileName } = await this.fileProcessor(fileBuffer, mime);
+    const fileUpload = await this.fileManager.uploadFile(filePath, {
+      mimeType: mime,
+      displayName: fileName,
     });
-  }
 
+    let fileState = await this.fileManager.getFile(fileUpload.file.name);
+    while (fileState.state === FileState.PROCESSING) {
+      logger.info("Loading file for ready state...");
+      await new Promise((res) => setTimeout(res, 5000));
+      fileState = await this.fileManager.getFile(fileUpload.file.name);
+    }
+    if (fileState.state === FileState.FAILED) {
+      throw new Error("File processing failed!");
+    }
+    return fileUpload;
+  }
 }
+
+module.exports = { GeminiFileAPI };
